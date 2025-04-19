4629cf2b1fb16690b606b87be2c555463b856f17
wangshijun <wangshijun2010@gmail.com>
2025-04-19 12:06:05 +0800

    chore: add access logger for proxy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require('dotenv-flow').config();
 
 const express = require('express');
+const logger = require('@blocklet/logger');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const env = require('./libs/env');
@@ -8,6 +9,7 @@ const env = require('./libs/env');
 const app = express();
 
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3030;
+logger.setupAccessLogger(app);
 
 app.use(createProxyMiddleware({ target: env.upstreamUrl, changeOrigin: false, followRedirects: false, ws: true }));
 
