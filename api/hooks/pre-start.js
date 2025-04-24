b2b9274114c06a3002356b573a9461bff144b5f0
JianChao Ye <guhuo13@gmail.com>
2025-04-24 11:08:52 +0800

    feat: support configuration of multiple URL mappings (#4)
    * feat: support configuration of multiple URL mappings
    
    * chore: bump version

--- api/hooks/pre-start.js
+++ api/hooks/pre-start.js
@@ -1,11 +1,10 @@
 require('@blocklet/sdk/lib/error-handler');
 require('dotenv-flow').config();
-
-const isUrl = require('is-absolute-url');
+const { mappings } = require('../libs/env');
 
 (() => {
-  if (isUrl(process.env.UPSTREAM_URL)) {
-    console.info(`Proxy to upstream: ${process.env.UPSTREAM_URL}`);
+  if (Array.isArray(mappings)) {
+    console.info(`Proxy to upstream:`, mappings);
     process.exit(0);
   } else {
     console.error('UPSTREAM_URL must be an absolute URL');
